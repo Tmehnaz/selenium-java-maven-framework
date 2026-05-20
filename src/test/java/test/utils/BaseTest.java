649d84085cad9cf34048621ea2e5c2f613f8fa68
Tmehnaz <87015782+Tmehnaz@users.noreply.github.com>
2026-05-20 12:11:46 +0300

    Update BaseTest.java

--- src/test/java/test/utils/BaseTest.java
+++ src/test/java/test/utils/BaseTest.java
@@ -13,51 +13,47 @@ import org.testng.annotations.BeforeClass;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class BaseTest{
+public class BaseTest {
+
     protected WebDriver driver;
     protected Properties config;
 
-@BeforeClass
-public void setUp() throws Exception {
-    config = new Properties();
-    FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
-    config.load(fis);
+    @BeforeClass
+    public void setUp() throws Exception {
+        config = new Properties();
+        FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+        config.load(fis);
 
-    String browser = config.getProperty("browser");
-    boolean headless = Boolean.parseBoolean(config.getProperty("headless"));
+        String browser = config.getProperty("browser");
+        boolean headless = Boolean.parseBoolean(config.getProperty("headless"));
 
-    if (browser.equalsIgnoreCase("chrome")){
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        if(headless) {
-    options.addArguments("--headless");
-    options.addArguments("--no-sandbox");           // required on Linux CI
-    options.addArguments("--disable-dev-shm-usage"); // prevents Chrome crashes on CI
-    options.addArguments("--window-size=1920,1080"); // replaces --start-maximized in headless
-} else {
-    options.addArguments("--start-maximized");
-}
-options.addArguments("--disable-notifications");
-        driver = new ChromeDriver(options);
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
 
-    }else if(browser.equalsIgnoreCase("firefox")){
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--disable-notifications");
+
+            driver = new ChromeDriver(options);
+
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+
+        driver.manage().timeouts().implicitlyWait(
+            Duration.ofSeconds(Integer.parseInt(config.getProperty("implicitWait")))
+        );
+        driver.get(config.getProperty("baseUrl"));
     }
 
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(
-         Integer.parseInt(config.getProperty("implicitWait"))
-        )
-        );
-        driver.manage().window().maximize();
-        driver.get(config.getProperty("baseUrl"));
-}
-@AfterClass
-public void tearDown() {
+    @AfterClass
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
     }
-
-
 }
